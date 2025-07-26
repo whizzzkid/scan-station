83a987b320bb98759fa6124a51cb5c88625b4474
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2025-07-26 14:44:38 -0600

    fix(scan): fix missing pages in scans. (#4)
    * fix(scan): fix missing pages in scans.
    
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
    
    * fix: suggestions.
    
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- scan.sh
+++ scan.sh
@@ -7,5 +7,21 @@ vendor=${vendor:-"fujitsu"}
 dpi=${dpi:-300}
 mode=${mode:-"Color"}
 
-# Run the scanning command
-/app/sane-scan-pdf/scan -v -d -x $vendor -r $dpi --mode $mode --skip-empty-pages -o /scans/$file_prefix-$now.pdf
+# Run the scanning command and save the output to a temporary file
+/app/sane-scan-pdf/scan -v -d -x $vendor -r $dpi --mode $mode --skip-empty-pages -o /tmp/$file_prefix-$now.pdf
+
+if [ $? -ne 0 ]; then
+    echo "Error: Scan command failed. Exiting."
+    exit 1
+fi
+
+# Once the scan is complete, move the file to the output directory
+echo "Scan completed successfully. Moving file to /scans/$file_prefix-$now.pdf"
+mv /tmp/$file_prefix-$now.pdf /scans/$file_prefix-$now.pdf
+
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to move the file to /scans. Please check permissions or directory existence." >&2
+    exit 1
+fi
+
+# This is needed to ensure the file is ready to be imported by the next step.
